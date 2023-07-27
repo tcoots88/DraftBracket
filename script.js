e2d88862973642b739336b0401a76fac4d77219d
Travis Coots <57024574+tcoots88@users.noreply.github.com>
2023-07-27 02:54:55 +0000

    new test for bracket

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-// Sample data for the 16-person tournament bracket
-const tournamentData = [
-    { round: 1, match: 1, teamA: "Person 1", teamB: "Person 2", winner: "Person 1" },
-    // Add the remaining matches for Round 1 here
-];
+// For the theme switcher / unneeded JS
 
-document.addEventListener("DOMContentLoaded", function () {
-    const tournamentBracket = document.querySelector(".tournament-bracket");
-
-    tournamentData.forEach((matchData) => {
-        const roundDiv = tournamentBracket.querySelector(`.round:nth-child(${matchData.round})`);
-        const matchDiv = document.createElement("div");
-        matchDiv.classList.add("match");
-
-        const teamADiv = document.createElement("div");
-        teamADiv.classList.add("team");
-        teamADiv.textContent = matchData.teamA;
-
-        const teamBDiv = document.createElement("div");
-        teamBDiv.classList.add("team");
-        teamBDiv.textContent = matchData.teamB;
-
-        if (matchData.winner === matchData.teamA) {
-            teamADiv.classList.add("winner");
-        } else if (matchData.winner === matchData.teamB) {
-            teamBDiv.classList.add("winner");
-        }
-
-        matchDiv.appendChild(teamADiv);
-        matchDiv.appendChild(teamBDiv);
-        roundDiv.appendChild(matchDiv);
-    });
+$(document).ready(function() {
+	$('#theme-none').click(function(e) {
+		$('.theme').removeClass('theme-light');
+		$('.theme').removeClass('theme-dark');
+		$('.theme').removeClass('theme-dark-trendy');
+	});
+	$('#theme-light').click(function(e) {
+		$('.theme').addClass('theme-light');
+		$('.theme').removeClass('theme-dark');
+		$('.theme').removeClass('theme-dark-trendy');
+	});
+	$('#theme-dark').click(function(e) {
+		$('.theme').removeClass('theme-light');
+		$('.theme').addClass('theme-dark');
+		$('.theme').removeClass('theme-dark-trendy');
+	});
+	$('#theme-dark-trendy').click(function(e) {
+		$('.theme').removeClass('theme-light');
+		$('.theme').removeClass('theme-dark');
+		$('.theme').addClass('theme-dark-trendy');
+	});
 });
